f116afe04af7903b34ddaf9e78366501ceafc3f2
PyPy <skfelix1@gmail.com>
2022-01-29 18:31:59 -0800

    adds links to footer

--- components/Footer.js
+++ components/Footer.js
@@ -18,10 +18,12 @@ function Footer() {
                 </div>
 
                 <div className='flex flex-col space-y-4'>
-                    <a className="text-gray-500 hover:text-gray-600 transition" href="/">GitHub</a>
+                    <a className="text-gray-500 hover:text-gray-600 transition" target="_blank"
+                    rel="noreferrer" href="https://github.com/GarrettGarrett">GitHub</a>
                 </div>
                 <div className='flex flex-col space-y-4'>
-                    <a className="text-gray-500 hover:text-gray-600 transition" href="/">LinkedIn</a>
+                    <a className="text-gray-500 hover:text-gray-600 transition" target="_blank"
+                    rel="noreferrer" href="https://www.linkedin.com/in/garrettfelix/">LinkedIn</a>
                 </div>
                 
         </div>
