51495365ae4f04a292ed93ca1764966e3b9ca7ee
PyPy <skfelix1@gmail.com>
2022-01-29 18:37:28 -0800

    uses next Link in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 function Footer() {
@@ -6,11 +7,15 @@ function Footer() {
         <div className='mt-28 border-t border-gray-800 mb-12'></div>
             <div className=' w-full max-w-2xl grid grid-cols-5 gap-4 pb-16 sm:grid-cols-5'>
                 <div className='flex flex-col space-y-4'>
-                    <a className="text-gray-500 hover:text-gray-600 transition" href="/">Timeline</a>
+                    <Link href="/timeline">
+                    <a className="text-gray-500 hover:text-gray-600 transition" >Timeline</a>
+                    </Link>
                 </div>
 
                 <div className='flex flex-col space-y-4'>
-                    <a className="text-gray-500 hover:text-gray-600 transition" href="/">Projects</a>
+                    <Link href='/projects'>
+                    <a className="text-gray-500 hover:text-gray-600 transition" >Projects</a>
+                    </Link>
                 </div>
 
                 <div className='flex flex-col space-y-4'>
