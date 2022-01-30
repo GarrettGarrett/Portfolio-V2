bb92c115caef6eb1f97b64a047f5bf45e20b6328
PyPy <skfelix1@gmail.com>
2022-01-29 18:40:29 -0800

    typo

--- components/MyProfile.js
+++ components/MyProfile.js
@@ -9,7 +9,7 @@ export default function MyProfile() {
         <div className="flex flex-col pr-8">
           <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-white dark:text-white">Garrett Felix</h1>
           <h2 className=" text-gray-200 mb-4">Full Stack Web Developer</h2>
-          <p className= "text-gray-400 mb-16">I my use knowledge of programming to find creative and unique solutions to complex problems motivated by the desire to help others.</p>
+          <p className= "text-gray-400 mb-16">I use my knowledge of programming to find creative and unique solutions to complex problems motivated by the desire to help others.</p>
         </div>
 
 
