78ca407cb295b890cfa6be691b035385ba2b675b
PyPy <skfelix1@gmail.com>
2022-08-23 15:10:51 -0700

    ga debug

--- pages/_app.js
+++ pages/_app.js
@@ -22,21 +22,20 @@ function MyApp({ Component, pageProps }) {
   
   return (
   <>
+
     <Script
-      strategy="lazyOnload"
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
     />
 
-    <Script strategy="lazyOnload">
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-          page_path: window.location.pathname,
-        });
-      `}
-    </Script>  
+    <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+    </Script>
 
     <ThemeProvider attribute="class">
       <Layout>
